891a3e5f9b2c7a852287940365de18b7756bafb6
Pendem Ganesh <pendemganesh2003@gmail.com>
2024-06-05 18:36:48 +0530

    Created Rubicks Cube

---
+++ Rubickscube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pende on 05-06-2024.
+//
+
+#include "Rubickscube.h"
---
+++ Rubickscube.h
@@ -0,0 +1,16 @@
+//
+// Created by pende on 05-06-2024.
+//
+
+#ifndef RUBICKSCUBE_H
+#define RUBICKSCUBE_H
+
+
+
+class Rubickscube {
+
+};
+
+
+
+#endif //RUBICKSCUBE_H
